312767d68c15407aa30c3a361d6b4c0698b27388
Niamh Madden <niamhmadden15@gmail.com>
2022-05-19 10:19:23 +0100

    boredBot complete

---
+++ boredBot/index.js
@@ -0,0 +1,15 @@
+
+    document.getElementById('button').addEventListener( 'click', function(){
+        fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => {
+          
+            console.log(data)
+            document.getElementById("activity-name").textContent=data.activity
+        })
+    })
+
+
+
+
+
